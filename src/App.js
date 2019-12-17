4bec82cfd8c25ef5c62953cbcdf71b4f3e42c179
snyarde <57542514+snyarde@users.noreply.github.com>
2019-12-17 14:49:44 -0500

    Cleanup
    Cleans up default project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+import ReactDOM from "react-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>PASH React Material UI</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
