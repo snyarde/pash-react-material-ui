e87a2bd6b413d77280d308f6cbd8c257c3596f3e
snyarde <57542514+snyarde@users.noreply.github.com>
2019-12-17 15:50:45 -0500

    Adds Material UI Simple Table
    Adds the Material UI simple table https://material-ui.com/components/tables/

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { Component, Fragment } from "react";
-import ReactDOM from "react-dom";
+import React, { Component } from "react";
+import StockItems from "./Components/StockItems";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>PASH React Material UI</h1>
+        <StockItems />
       </div>
     );
   }
